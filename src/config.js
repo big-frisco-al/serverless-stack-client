ba4fd64486e7b063dd97a21a14ce1a14e586a889
Alan Sit <sit.alan@gmail.com>
2021-02-22 08:20:05 -0800

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 const config = {
+  STRIPE_KEY: "pk_test_51INc64AHVtOaLLfLlyjTfNGbf3Np23l1rU4Kbp6ojoU3aQ1PwejAG6bRLj0K0mvUBrhMaXOHuJN8ysjrIOcdy89y00LjmFuQuv",
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-west-1",
     BUCKET: "sdfkasjhflkwjehrlasnf",
@@ -10,7 +12,7 @@ const config = {
   cognito: {
     REGION: "us-west-1",
     USER_POOL_ID: "us-west-1_GRATDlwGa",
-    APP_CLIENT_ID: "sdfkasjhflkwjehrlasnf",
+    APP_CLIENT_ID: "5g76vtmcoc751mr86tle6u4fgs",
     IDENTITY_POOL_ID: "us-west-1:7b053684-61e9-4af5-be98-c1620fb3eac8",
   },
 };
